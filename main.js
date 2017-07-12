8c93be20045bb43912bd90b9574133139538d431
Nandaja <nandaja.varma@gmail.com>
2017-07-13 02:37:11 +0530

    And it completes...

--- main.js
+++ main.js
@@ -5,26 +5,8 @@ $(document).ready(function(){
             //var msg = new SpeechSynthesisUtterance(text);
             //window.speechSynthesis.speak(msg);
     //}
-    $("input").keyup(function(e) {
-            //let readme = true;
-
-            //if(e.keyCode == 8){
-                        //secrets.pop();
-                    //}
-            //else if(e.key == "Enter"){
-                        //submitted();
-                        //readme = false;
-                    //}
-            //else if(e.key.length == 1){
-                        //secrets.push(e.key);
-                    //}
-
-            //if(readme){
-                    //readout(e.key);
-                    //}
-
-            console.log(e);
-            //console.log(secrets.join(""));
+    $("input").autocomplete({
+        source: ["Nandaja", "smells", "funny", "dom", "hates", "cats"]
     });
 
 
